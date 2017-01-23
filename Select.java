f716e9faf264436b08e3f3ff41b5d85ceac9aecf
Jake Woodlee <jwoodlee@lion.lmu.edu>
2017-01-22 20:05:43 -0800

    added git ignore, and created stdio stuff for Select.java

--- Select.java
+++ Select.java
@@ -1,3 +1,22 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Select {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader stdIn = new java.io.BufferedReader ( new java.io.InputStreamReader ( System.in ) );
+        String s = stdIn.readLine();// grab the first line (or null, if end-of-file)
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        int i = 0;
+        while ( s != null ) { // while not end-of-file
+            numList.add(Integer.parseInt(s));
+            s = stdIn.readLine(); // grab the next line (or null)
+            System.out.println(numList.get(i));
+            i++;
+        }
+    }
+
+    public static int findNthLargest (ArrayList<Integer> numList, int n) {
+        return 0;
+    }
+
 }
