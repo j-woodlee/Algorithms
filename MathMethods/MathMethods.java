f28358ee617eb3aa563c02a7a6df8f87012dba02
Jake Woodlee <jwoodlee@lion.lmu.edu>
2017-02-13 19:20:25 -0800

    added fib and factorial methods to MathMethods.java

--- MathMethods/MathMethods.java
+++ MathMethods/MathMethods.java
@@ -2,27 +2,47 @@ import java.math.BigInteger;
 
 public class MathMethods {
 
-    public MathMethods() {
+    public static void main(String[] args) {
+        // System.out.println(factorial(4));
+        // System.out.println(fibonacci(7));
 
     }
 
     static BigInteger factorial(int n) {
-        return new BigInteger("");
+        if (n == 1 || n == 0) {
+            return new BigInteger("1");
+        }
+        // compareTo: -1, 0 or 1 as this BigInteger is numerically less than, equal to, or greater than val.
+        BigInteger nBigInt = new BigInteger(n + "");
+        BigInteger product = new BigInteger("1");
+
+        while(nBigInt.compareTo(BigInteger.ZERO) > 0) {
+            product = product.multiply(nBigInt);
+            // System.out.println(product);
+            nBigInt = nBigInt.subtract(new BigInteger("1"));
+        }
+        return product;
     }
 
     static BigInteger fibonacci(int n) {
-        return new BigInteger("");
+        BigInteger nminus1th = BigInteger.ZERO;
+        BigInteger nth = BigInteger.ONE;
+
+        BigInteger next;
+        for (int i = 0; i < n; i++) {
+            next = nminus1th.add(nth);
+            nminus1th = nth;
+            nth = next;
+        }
+        return nth;
     }
 
     static long gcd(long m, long n) {
         return 0;
     }
+    
     static long lcm(long m, long n) {
         return 0;
-    }
-
-    static void main(java.lang.String[] args) {
-        return;
     }
 
     static double poly(double x, double[] coeff) {
