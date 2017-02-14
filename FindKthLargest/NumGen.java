f008a6cab8441824c872b99b77627a9fabfb9cba
Jake Woodlee <jwoodlee@lion.lmu.edu>
2017-02-13 18:17:32 -0800

    made some folders and added MathMethods.java

---
+++ FindKthLargest/NumGen.java
@@ -0,0 +1,7 @@
+public class NumGen {
+    public static void main(String[] args) {
+            for (int i = 1; i <= 5000000; i++) {
+                System.out.println((int)(Math.random() * 1000000) + 1);
+            }
+    }
+}
---
+++ MathMethods/MathMethods.java
@@ -0,0 +1,44 @@
+import java.math.BigInteger;
+
+public class MathMethods {
+
+    public MathMethods() {
+
+    }
+
+    static BigInteger factorial(int n) {
+        return new BigInteger("");
+    }
+
+    static BigInteger fibonacci(int n) {
+        return new BigInteger("");
+    }
+
+    static long gcd(long m, long n) {
+        return 0;
+    }
+    static long lcm(long m, long n) {
+        return 0;
+    }
+
+    static void main(java.lang.String[] args) {
+        return;
+    }
+
+    static double poly(double x, double[] coeff) {
+        return 0;
+    }
+
+    static double power(double x, int n) {
+        return 0;
+    }
+
+    static double root(int n, double x, double epsilon) {
+        return 0;
+    }
+
+    static double sqrt(double x, double epsilon) {
+        return 0;
+    }
+
+}
