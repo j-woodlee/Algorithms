2c7531483c6b4dc9ae50cec1a73d2f469d1d1a3f
Jake Woodlee <jwoodlee@lion.lmu.edu>
2017-02-28 03:17:08 -0800

    after a long session of coding I could be potentially done with this, need to do more testing

--- SumoSolver/SumoSolver.java
+++ SumoSolver/SumoSolver.java
@@ -1,5 +1,61 @@
 
 import java.util.HashMap;
+import java.util.ArrayList;
+
+class Item { // courtesy of Ryan
+    int cost;
+    int weight;
+    Item (int cost, int weight) {
+        this.cost = cost;
+        this.weight = weight;
+    }
+    public String toString() {
+        return "(" + cost + ", " + weight + ")";
+    }
+}
+
+class ItemList {
+
+    ArrayList<Item> items = new ArrayList<Item>();
+
+    ItemList() {
+        this.items = new ArrayList<Item>();
+    }
+
+    ItemList(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public ItemList addItem(Item item) {
+        items.add(item);
+        return this;
+    }
+
+    public int getTotalCost() {
+        int sum = 0;
+        for (Item i : items) {
+            sum += i.cost;
+        }
+        return sum;
+    }
+
+    public int getTotalWeight() {
+        int sum = 0;
+        for (Item i : items) {
+            sum += i.weight;
+        }
+        return sum;
+    }
+
+    public ArrayList<Item> getItems() {
+        return this.items;
+    }
+
+    public String toString() {
+        return this.items.toString();
+    }
+}
+
 
 public class SumoSolver {
     public static void main(String[] args) {
@@ -28,8 +84,12 @@ public class SumoSolver {
             }
         }
         int budget = Integer.parseInt(args[args.length - 1]);
-        Integer[][] solvedSols = new Integer[prices.length][budget];
-        System.out.println(optimize(prices, weights, budget, prices.length, solvedSols));
+        ItemList[][] solvedSols = new ItemList[prices.length][budget];
+        Item[] itemArray = new Item[prices.length];
+        for(int i = 0; i < itemArray.length; i++) {
+            itemArray[i] = new Item(prices[i], weights[i]);
+        }
+        System.out.println(optimize(prices, weights, budget, prices.length, solvedSols, itemArray));
     }
 
     public static void usage() {
@@ -37,9 +97,10 @@ public class SumoSolver {
             "java SumoSolver 48 51 49 52 55 99 100");
     }
 
-    public static int optimize(int[] prices, int[] weights, int remainingMoney, int items, Integer[][] solvedSols) {
+    public static ItemList optimize(int[] prices, int[] weights, int remainingMoney, int items, ItemList[][] solvedSols, Item[] itemArray) {
+
         if (remainingMoney == 0 || items == 0) {
-            return 0;
+            return new ItemList();
         }
 
         if (solvedSols[items - 1][remainingMoney - 1] != null) { // returned memoized value
@@ -47,18 +108,18 @@ public class SumoSolver {
         }
 
         if (prices[items - 1] > remainingMoney) {
-            int answer = optimize(prices, weights, remainingMoney, items - 1, solvedSols);
+            ItemList answer = optimize(prices, weights, remainingMoney, items - 1, solvedSols, itemArray);
             solvedSols[items - 1][remainingMoney - 1] = answer;
             return answer;
         } else {
-            int possibility1 = weights[items - 1] + optimize(prices, weights, remainingMoney - prices[items - 1], items - 1, solvedSols);
-            int possibility2 = optimize(prices, weights, remainingMoney, items - 1, solvedSols);
-            int answer = Math.max(possibility1, possibility2);
-            if (answer == possibility1) {
-                System.out.println("weight: " + weights[items - 1]+ " price: " + prices[items - 1]);
-            }
+            ItemList possibility1 = new ItemList(optimize(prices, weights, remainingMoney - prices[items - 1], items - 1, solvedSols, itemArray).getItems()).addItem(itemArray[items - 1]);
+            ItemList possibility2 = new ItemList(optimize(prices, weights, remainingMoney, items - 1, solvedSols, itemArray).getItems());
+            ItemList answer = max(possibility1, possibility2);
             solvedSols[items - 1][remainingMoney - 1] = answer;
             return answer;
         }
+    }
+    public static ItemList max(ItemList one, ItemList two) {
+        return one.getTotalWeight() >= two.getTotalWeight() ? one : two;
     }
 }
